21a92907a26ea05543c02a9544c5a553120dabde
Chris de Graaf <chrisadegraaf@gmail.com>
2016-09-25 13:57:34 -0500

    holy shit it's working

--- frontend.py
+++ frontend.py
@@ -9,37 +9,87 @@ class State(abc.ABC):
         return
 
 
-class SearchResultsState(State):
+class ExpandableState(State):
+    def __init__(self, options):
+        self.options = options
+
+    @abc.abstractclassmethod
+    def length(self):
+        # get the length of the list of options
+        return
+
+    @abc.abstractclassmethod
+    def get_option(self, num):
+        return
+
+
+class SearchResultsState(ExpandableState):
     def __init__(self, search_results):
-        self.search_results = search_results
+        super().__init__(search_results)
 
     def length(self):
-        return sum([len(self.search_results[key]) for key in self.search_results])
+        return sum([len(self.options[key]) for key in self.options])
 
     def show(self):
         i = 1
-        for key in self.search_results:
+        for key in sorted(self.options):  # albums, artists, songs
             print('%s:' % key.capitalize())
-            for result in self.search_results[key]:
+            for result in self.options[key]:
                 print('%d: %s' % (i, result.to_string()))
                 i += 1
 
+    def get_option(self, num):
+        count = 1
+        for key in sorted(self.options):
+            for item in self.options[key]:
+                if count is num:
+                    return item
+                else:
+                    count += 1
 
-class ShowObjectState(State):
+
+class ShowObjectState(ExpandableState):
     def __init__(self, music_obj):
-        self.music_obj = music_obj
+        super().__init__(music_obj)
 
     def length(self):  # get the length of all whatever list is being shown
-        return self.music_obj.length()
+        return self.options.length()
 
     def show(self):
-        self.music_obj.show()
+        self.options.show()
+
+    def get_option(self, num):
+        count = 1
+        if count is num:
+            return self.options
+        else:
+            count += 1
+            for key in self.options.contents:
+                for item in self.options.contents[key]:
+                    if count is num:
+                        if item['id'].startswith('A'):
+                            return pmcli.Artist(pmcli.API.api.get_artist_info(item['id']))
+                        elif item['id'].startswith('B'):
+                            return pmcli.Album(pmcli.API.api.get_album_info(item['id']))
+                        elif item['id'].startswith('T'):
+                            return pmcli.Song(pmcli.API.api.get_track_info(item['id']))
+                    else:
+                        count += 1
+
+        # for key in sorted(self.options):
+        #     for item in self.options[key]:
+        #         if count is num:
+        #             if item['id'].startswith('A'):
+        #                 return pmcli.Artist(item['id'])
+        #             elif item[key]['id'].startswith('B'):
+        #                 return pmcli.Album(item['id'])
+        #             elif item[key]['id'].startswith('T'):
+        #                 return pmcli.Song(item['id'])
+        #         else:
+        #             count += 1
 
 
 class HelpState(State):
-    def __init__(self):
-        pass
-
     def show(self):
         print('Commands:')
         print('h/help: Show this help message')
@@ -50,8 +100,8 @@ class HelpState(State):
 
 
 class InvalidInputState(State):
-    def __init__(self):
-        pass
+    def __init__(self, last_state):
+        self.last_state = last_state
 
     def show(self):
         print('Invalid input: enter h for help')
@@ -59,9 +109,11 @@ class InvalidInputState(State):
 
 def transition(current_state, user_input):
     # returns the new state
-    play_states = info_states = ['ShowObjectState',
-                                 'SearchResultsState']  # only allow play/info commands at these states
     user_input = [i.strip() for i in user_input.split(' ', 1)]
+
+    # restore old state if we had invalid input
+    if type(current_state).__name__ == 'InvalidInputState':
+        current_state = current_state.last_state
 
     # search
     if user_input[0] == 's' or user_input[0] == 'search':
@@ -69,29 +121,33 @@ def transition(current_state, user_input):
 
     # play
     elif user_input[0] == 'p' or user_input[0] == 'play':
-        if type(current_state).__name__ in play_states:
-            try:
-                num = int(user_input[1])
-
-            except ValueError:
-                next_state = InvalidInputState()
-        else:
-            next_state = InvalidInputState()
-
-    # info
-    elif user_input[0] == 'i' or user_input[0] == 'info':
-        if type(current_state).__name__ in info_states:
+        if isinstance(current_state, ExpandableState):
             try:
                 num = int(user_input[1])
                 if num > current_state.length():
-                    next_state = InvalidInputState()
+                    next_state = InvalidInputState(current_state)
                 else:
-                    pass
-
+                    current_state.get_option(num).play()
+                    next_state = current_state
             except ValueError:
-                next_state = InvalidInputState()
+                next_state = InvalidInputState(current_state)
         else:
-            next_state = InvalidInputState()
+            next_state = InvalidInputState(current_state)
+
+    # info
+    elif user_input[0] == 'i' or user_input[0] == 'info':
+
+        if isinstance(current_state, ExpandableState):  # sorry, python purists
+            try:
+                num = int(user_input[1])
+                if num > current_state.length():
+                    next_state = InvalidInputState(current_state)
+                else:
+                    next_state = ShowObjectState(current_state.get_option(num))
+            except ValueError:
+                next_state = InvalidInputState(current_state)
+        else:
+            next_state = InvalidInputState(current_state)
 
     # help
     elif user_input[0] == 'h' or user_input[0] == 'help':
@@ -103,15 +159,14 @@ def transition(current_state, user_input):
             quit()
         next_state = current_state
     else:
-        next_state = InvalidInputState()
+        next_state = InvalidInputState(current_state)
 
     return next_state
 
-# def get_list_entry(current_state, num):
 
 if __name__ == '__main__':
     api = pmcli.API()
-    print('Welcome to pmcli!')
+    print('Welcome to pmcli! Enter h for help.')
     state = None
     while True:
         state = transition(state, input('> '))
--- pmcli.py
+++ pmcli.py
@@ -49,18 +49,9 @@ class API:
 # ------------------------------------------------------------
 
 class MusicObject(abc.ABC):
-    def __init__(self, obj_id):
-        self.obj_id = obj_id
-
-    @abc.abstractmethod
-    def length(self):
-        # returns the number of a MusicObjects child objects, for example number of songs in an Album
-        return
-
-    @abc.abstractmethod
-    def append_ids(self):
-        # combines all child object ids into one list, for example all the songs in an album
-        return
+    def __init__(self, name, id):
+        self.name = name
+        self.id = id
 
     @abc.abstractmethod
     def to_string(self):
@@ -82,26 +73,65 @@ class MusicObject(abc.ABC):
 
 class Artist(MusicObject):
     def __init__(self, artist):
-        self.contents = {'name': artist['name'],
-                         'albums_ids': [album['albumId'] for album in artist['albums']],
-                         'albums': [album['name'] for album in artist['albums']],
-                         'song_ids': [song['storeId'] for song in artist['topTracks']],
-                         'songs': [song['title'] for song in artist['topTracks']]}
-        super().__init__(artist['artistId'])
+        self.contents = {}
+        if 'albums' in artist:
+            self.contents['albums'] = [{'name': album['name'], 'id': album['albumId']} for album in artist['albums']]
+        else:
+            self.contents['albums']= {}
+        if 'topTracks' in artist:
+            self.contents['songs'] = [{'name': song['title'], 'id': song['storeId']} for song in artist['topTracks']]
+        else:
+            self.contents['songs'] = {}
+        super().__init__(artist['name'], artist['artistId'])
 
     def length(self):
-        return len(self.contents['albums']) + len(self.contents['songs'])
-
-    def append_ids(self):
-        return self.contents['album_ids'] + self.contents['song_ids']
+        return sum([len(self.contents[key]) for key in self.contents])
 
     def to_string(self):
-        return self.contents['name']
+        return self.name
 
     def play(self):
         urls = []
         print('Getting stream URLs for %s:' % self.to_string())
-        urls = [API.api.get_stream_url(song_id) for song_id in self.contents['song_ids']]
+        urls = [API.api.get_stream_url(song['id']) for song in self.contents['songs']]
+        with open(os.path.join(os.path.expanduser('~'), '.config', 'pmcli', 'playlist'), 'w') as playlist:
+            for url in urls:
+                playlist.write("%s\n" % url)
+        print('Playing %s:' % self.to_string())
+        subprocess.call(['mpv', '-playlist', os.path.join(os.path.expanduser('~'), '.config', 'pmcli', 'playlist')])
+
+    def show(self):
+        count = 1
+        print('%d: %s' % (count, self.to_string()))
+        count += 1
+        print('Albums:')
+        for album in self.contents['albums']:
+            print('%d: %s' % (count, album['name']))
+            count += 1
+        print('Songs:')
+        for song in self.contents['songs']:
+            print('%d: %s' % (count, song['name']))
+            count += 1
+
+
+# ------------------------------------------------------------
+
+class Album(MusicObject):
+    def __init__(self, album):
+        self.contents = {}
+        self.contents = {'artist': {'name': album['artist'], 'id': album['artistId']},
+                         'songs': [{'name': song['title'], 'id': song['storeId']} for song in album['tracks']]}
+        super().__init__(album['name'], album['albumId'])
+
+    def length(self):
+        return len(self.contents['songs'])
+
+    def to_string(self):
+        return ' - '.join((self.contents['artist']['name'], self.name))
+
+    def play(self):
+        print('Getting stream URLS for %s:' % self.to_string())
+        urls = [API.api.get_stream_url(song['id']) for song in self.contents['songs']]
         with open(os.path.join(os.path.expanduser('~'), '.config', 'pmcli', 'playlist'), 'w') as playlist:
             for url in urls:
                 playlist.write("%s\n" % url)
@@ -111,53 +141,10 @@ class Artist(MusicObject):
     def show(self):
         count = 1
         print(self.to_string())
-        print('Songs:')
-        for song in self.contents['songs']:
-            print('%d: %s' % (count, song.capitalize()))
-            count += 1
-        print('Albums:')
-        for album in self.contents['albums']:
-            print('%d: %s' % (count, album.capitalize()))
-            count += 1
-
-
-# ------------------------------------------------------------
-
-class Album(MusicObject):
-    def __init__(self, album):
-        self.contents = {'name': album['name'],
-                         'artist': album['artist'],
-                         'artist_id': album['artistId'],
-                         'songs': [song['title'] for song in album['tracks']],
-                         'song_ids': [song['storeId'] for song in album['tracks']]}
-        super().__init__(album['albumId'])
-
-    def length(self):
-        return len(self.contents['songs'])
-
-    def append_ids(self):
-        return [self.obj_id] + [self.contents['artist_id']] + self.contents['song_ids']
-
-    def to_string(self):
-        return ' - '.join((self.contents['artist'], self.contents['name']))
-
-    def play(self):
-        print('Getting stream URLS for %s:' % self.to_string())
-        urls = [API.api.get_stream_url(song_id) for song_id in self.contents['song_ids']]
-        playlist = open(os.path.join(os.path.expanduser('~'), '.config', 'pmcli', 'playlist'), 'w')
-        for url in urls:
-            playlist.write("%s\n" % url)
-        playlist.close()
-        print('Playing %s:' % self.to_string())
-        subprocess.call(['mpv', '-playlist', os.path.join(os.path.expanduser('~'), '.config', 'pmcli', 'playlist')])
-
-    def show(self):
-        count = 1
-        print('%d: %s' % (count, self.to_string()))
         count += 1
         print('Songs:')
         for song in self.contents['songs']:
-            print('%d: %s' % (count, song.capitalize()))
+            print('%d: %s' % (count, song['name']))
             count += 1
 
 
@@ -165,28 +152,22 @@ class Album(MusicObject):
 
 class Song(MusicObject):
     def __init__(self, song):
-        self.contents = {'name': song['title'],
-                         'artist_id': song['artistId'][0],
-                         'artist': song['artist'],
-                         'album_id': song['album'],
-                         'album': song['album']}
-        super().__init__(song['storeId'])
+        self.contents = {'artist': {'name': song['artist'], 'id': song['artistId'][0]},
+                         'album': {'name': song['album'], 'id': song['albumId']}}
+        super().__init__(song['title'], song['storeId'])
 
     def length(self):
         return 1
 
-    def append_ids(self):
-        return self.obj_id
-
     def to_string(self):
-        return ' - '.join((self.contents['artist'], self.contents['name'], self.contents['album']))
+        return ' - '.join((self.contents['artist']['name'], self.name, self.contents['album']['name']))
 
     def play(self):
         print('Getting stream URL:')
-        url = API.api.get_stream_url(self.obj_id)
+        url = API.api.get_stream_url(self.id)
         print('Playing %s:' % self.to_string())
         subprocess.call(['mpv', '-really-quiet', url])
 
     def show(self):
         count = 1
-        print('%d: %s' % self.to_string())
+        print('%d: %s' % (count, self.to_string()))
