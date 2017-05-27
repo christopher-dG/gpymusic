f7c9cf3f805192c81c696e572d5c04a03c9bc6c5
Chris de Graaf <chrisadegraaf@gmail.com>
2017-05-27 10:39:32 -0500

    Remove script directory from setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='gpymusic',
     version='0.0.5',
-    packages=find_packages(exclude=['bin', 'script']),
+    packages=find_packages(exclude=['bin']),
     scripts=[
         'bin/gpymusic',
         'bin/gpymusic-setup',
