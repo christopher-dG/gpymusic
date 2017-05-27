3be6f5331036f061df15bb788be0a5fba1f1f52b
Chris de Graaf <chrisadegraaf@gmail.com>
2017-05-27 11:01:32 -0500

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gpymusic',
-    version='0.0.5',
+    version='0.0.6',
     packages=find_packages(exclude=['bin']),
     scripts=[
         'bin/gpymusic',
