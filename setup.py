8aea0d54029ca363bc907b28686b6e9260e41893
Chris de Graaf <chrisadegraaf@gmail.com>
2017-05-28 01:06:29 -0500

    Bump to version v1.0.0

--- setup.py
+++ setup.py
@@ -1,9 +1,38 @@
 from setuptools import setup, find_packages
 
+import sys
+
+
+if sys.version_info < (3, 4):
+    print('gpymusic requires python>=3.4.')
+    exit(1)
+
 setup(
     name='gpymusic',
-    version='0.0.6',
+    version='1.0.0',
+    description='A simple TUI client for Google Play Music',
+    url='https://github.com/christopher-dG/gpymusic',
+    author='Chris de Graaf',
+    author_email='chrisadegraaf@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Environment :: Console :: Curses',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'Operating System :: Unix',
+        'Topic :: Multimedia :: Sound/Audio :: Players',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+    keywords='terminal music streaming',
     packages=find_packages(exclude=['bin']),
+    install_requires=['gmusicapi'],
+    package_dir={'gpymusic': 'gpymusic'},
+    package_data={'gpymusic': ['config/*']},
     scripts=[
         'bin/gpymusic',
         'bin/gpymusic-setup',
@@ -11,9 +40,4 @@ setup(
         'bin/gpymusic-get-dev-id',
         'bin/gpymusic-oauth-login'
     ],
-    install_requires=['gmusicapi'],
-    package_dir={'gpymusic': 'gpymusic'},
-    package_data={
-        'gpymusic': ['config/*']
-    }
 )
